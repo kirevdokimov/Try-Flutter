0f0d3315529655342335534a55c4c9ce5a84ee24
Nobinator <nobinator.root@gmail.com>
2018-04-14 05:02:33 +0300

    Startup name generator

--- lib/RandomWordsApp.dart
+++ lib/RandomWordsApp.dart
@@ -7,10 +7,13 @@ class RandomWordsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'RandomWordsAppTitle',
+
+// Меняем тему
+//      theme: new ThemeData(
+//        primaryColor: Colors.white,
+//      ),
+
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Random Words App'),
-        ),
         body: new Center(
           child: new RandomWords(),
         ),
@@ -37,12 +40,58 @@ class RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildSuggestions()
-    => new ListView.builder(
+  void nothing(){}
+
+  void _pushSaved() {
+    // Navigator управляет перемещениями между экранами. Сейчас мы вбрасываем
+    // ему Scaffold со списком сохраненных элементов списка
+    Navigator.of(context).push(getMaterialRoute(context));
+  }
+
+  PageRoute getMaterialRoute(BuildContext context){
+    return new MaterialPageRoute(
+      builder: (context) {
+
+        // ListTile
+        final tiles = _saved.map(
+              (pair) {
+            return new ListTile(
+              title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+
+        final divided = ListTile
+            .divideTiles(context: context,tiles: tiles).toList();
+
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Saved Suggestions'),
+          ),
+          body: new ListView(children: divided),
+        );
+
+
+      },
+    ));
+  }
+
+  Widget _buildSuggestions() => new ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return new Divider();
@@ -54,7 +103,6 @@ class RandomWordsState extends State<RandomWords> {
           return _buildRow(_suggestions[index]);
         }
     );
-
 
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
